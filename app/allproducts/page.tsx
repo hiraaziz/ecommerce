efc6b66baadffcda07a15015c49800494cdebdc4
hiraaziz <hiraaziz1998@gmail.com>
2023-06-19 14:35:51 +0500

    Update page.tsx

--- app/allproducts/page.tsx
+++ app/allproducts/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { SanityProducts } from "@/type";
 
-export const getProdData = async () => {
+const getProdData = async () => {
   const res: SanityProducts[] = await client.fetch(`*[_type=="product"]{
     _id,
     images,
