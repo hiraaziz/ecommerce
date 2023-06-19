066e841c452b8445b02ed0332004b2e1e58d4243
hiraaziz <hiraaziz1998@gmail.com>
2023-06-19 15:17:15 +0500

    Update route.ts

--- app/api/create-stripe-session/route.ts
+++ app/api/create-stripe-session/route.ts
@@ -32,7 +32,7 @@ export async function POST(req: any, res: NextResponse) {
   const redirectURL =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "your deployed url";
+      : "https://hira-ecommerce.vercel.app/";
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
